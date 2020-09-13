f5e369c40b3742db1b767c19fbeff4267012f7a7
Siddy <sid.cd.varma@gmail.com>
2020-09-13 10:24:02 +0530

    completed project and deployed on firebase

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,10 +1,13 @@
 import React,{useState, useEffect} from 'react'
 import axios from '../axios';
 import './MovieRow.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
 
 const base_url="https://image.tmdb.org/t/p/original/";
 export const MovieRow = ({title,fetchUrl,isLarge}) => {
     const [movies,setMovies]=useState([])
+    const [trailerUrl,setTrailerUrl]=useState('')
     useEffect(()=>{
         async function fetchData(){
             const request=await axios.get(fetchUrl);
@@ -14,6 +17,34 @@ export const MovieRow = ({title,fetchUrl,isLarge}) => {
         }
         fetchData()
     },[fetchUrl])
+
+    const options={
+        height: "390",
+        widht: "100%",
+        playerVars:{
+            autoplay: 1,
+        }
+    }
+
+    const handleClick=(movie)=>{
+        console.log('inside handle click: ',movie)
+        if(trailerUrl)
+        setTrailerUrl('');
+        else{
+            const movieName=movie.name ? movie.name: movie.original_title ? movie.original_title : ""
+            if(!movieName) return;
+            movieTrailer(movieName)
+                .then(url=>{
+                    const urlParams=new URLSearchParams(new URL(url).search)
+                    console.log(urlParams)
+                    setTrailerUrl(urlParams.get("v"))
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+        }
+    }
+
     if(movies.length>0) console.log(movies);
     return (
         <div className="row">
@@ -23,13 +54,13 @@ export const MovieRow = ({title,fetchUrl,isLarge}) => {
                 {
                     movies.map(movie=>{
                         return(
-                            <img key={movie.id} className={`row__poster ${isLarge && "row__posterLarge"}`} src={`${base_url}${isLarge?movie.poster_path: movie.backdrop_path}`} alt={movie.name}/>
+                            <img onClick={()=>{handleClick(movie)}} key={movie.id} className={`row__poster ${isLarge && "row__posterLarge"}`} src={`${base_url}${isLarge?movie.poster_path: movie.backdrop_path}`} alt={movie.name}/>
                         )
                     })
                 }
             </div>
             {/* container with posters */}
-
+            { trailerUrl && <YouTube videoId={trailerUrl} opts={options} /> }
         </div>
     )
 }
